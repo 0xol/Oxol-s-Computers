70a005304d73a691149aed7d3f45cc8fc91302fd
0xol <86998622+0xol@users.noreply.github.com>
2023-04-01 15:46:43 +1000

    item registry

--- src/main/kotlin/oxol/oxolscomputers/RegisterItems.kt
+++ src/main/kotlin/oxol/oxolscomputers/RegisterItems.kt
@@ -1,9 +1,21 @@
 package oxol.oxolscomputers
 
-//import fabricmc.api
+import net.minecraft.util.Identifier
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.item.Item
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+
 
 object registerItems {
-    fun init() {
+    
+    fun registerItem(item: Item, name: String) {
+        Registry.register(Registries.ITEM, Identifier("oxolscomputers", name), item)
+    }
 
+    val CUSTOM_ITEM: Item = Item(FabricItemSettings())
+    
+    fun init() {
+        registerItem(CUSTOM_ITEM, "test")
     }
 }
